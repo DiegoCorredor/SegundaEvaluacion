1cad919ec19ab54d50d0cf52260cae20b6e6f966
Diegoalcor <diego.corredor.98@gmail.com>
2023-05-02 18:15:01 -0500

    Initial commit to Work

---
+++ src/main/java/com/uptc/parcial/ParcialDiegoCFreddyOApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialDiegoCFreddyOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialDiegoCFreddyOApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uptc/parcial/ParcialDiegoCFreddyOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialDiegoCFreddyOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
